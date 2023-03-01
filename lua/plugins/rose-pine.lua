0b4cedbb6a91e76198cb1c4207253da984b27a9f
jross <john.ross.dela.cruz@accenture.com>
2023-03-01 22:28:47 +0800

    removed transparent background for floating windows

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -5,6 +5,5 @@ return {
     require("rose-pine").setup({ disable_italics = true })
     vim.cmd('colorscheme rose-pine')
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFLoat", { bg = "none" })
   end
 }
