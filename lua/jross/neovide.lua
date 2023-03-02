461f4752c0037550a9314cff14c9ea3454ae922a
jross <john.ross.dela.cruz@accenture.com>
2023-03-02 17:28:13 +0800

    removed neovide config

--- lua/jross/neovide.lua
+++
@@ -1,7 +0,0 @@
-if vim.g.neovide then
-  vim.g.neovide_transparency = 0.75
-  vim.g.neovide_refresh_rate = 144
-  vim.g.neovide_refresh_rate_idle = 1
-  vim.g.neovide_cursor_trail_size = 0.5
-  vim.g.neovide_cursor_vfx_mode = "sonicboom"
-end
