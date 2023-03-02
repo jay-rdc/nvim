dfc38e7efcbf565fc3c695706d9147591c222971
jross <john.ross.dela.cruz@accenture.com>
2023-03-02 12:01:20 +0800

    added ensured languages in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,14 +5,17 @@ return {
 	  require 'nvim-treesitter.configs'.setup {
 		  -- A list of parser names, or 'all' (the four listed parsers should always be installed)
 		  ensure_installed = {
-			  'help',
-			  'javascript',
-			  'typescript',
-			  'lua',
-			  'rust',
-			  'markdown',
+        'css',
 			  'diff',
-			  'json'
+        'gitcommit',
+			  'help',
+        'html',
+			  'javascript',
+			  'json',
+			  'lua',
+			  'markdown',
+			  'rust',
+			  'typescript',
 		  },
 
 		  -- Install parsers synchronously (only applied to `ensure_installed`)
