64ef540eba04935383ee2078f2f23de66736f2bb
jross <delacruz.jross@gmail.com>
2023-03-02 14:23:19 +0800

    added grep search remap for telescope plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,6 @@
 return {
   'nvim-telescope/telescope.nvim',
-  version = '0.1.1',
+  branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim'
   },
@@ -10,5 +10,8 @@ return {
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    vim.keymap.set('n', '<leader>ps', function ()
+      builtin.grep_string({ search = vim.fn.input('Grep > ') })
+    end)
   end
 }
