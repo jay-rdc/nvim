2694ca13c645800e5545f01eb90ce182638f8899
jross <delacruz.jross@gmail.com>
2023-03-03 23:23:40 +0800

    edited statusline config

--- lua/jross/options.lua
+++ lua/jross/options.lua
@@ -22,4 +22,4 @@ vim.opt.scrolloff = 8
 
 vim.opt.updatetime = 50
 
-vim.opt.statusline = "%( 󰘬 %{FugitiveStatusline()} |%)%(  %t%)%( %m%)"
+vim.opt.statusline = "%( 󰘬 %{FugitiveHead()} |%)%(  %t%)%( %m%)"
