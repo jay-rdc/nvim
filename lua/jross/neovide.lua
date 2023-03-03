1ac9613928098d6c3e009f278d769e3deaec7aa0
jross <delacruz.jross@gmail.com>
2023-03-04 01:04:41 +0800

    added idle refresh rate in neovide config

--- lua/jross/neovide.lua
+++ lua/jross/neovide.lua
@@ -3,6 +3,7 @@ if vim.g.neovide then
   vim.g.neovide_transparency = 0.75
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_refresh_rate_idle = 1
   vim.g.neovide_cursor_animation_length = 0.08
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
