c2e0a23d9fe1c2fcd0452c1e440c7f60c52ecaa4
jross <delacruz.jross@gmail.com>
2023-03-03 23:14:34 +0800

    edited harpoon menu toggle remap

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -5,7 +5,7 @@ return {
     local ui = require("harpoon.ui")
 
     vim.keymap.set("n", "<leader>ha", mark.add_file)
-    vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+    vim.keymap.set("n", "<leader>he", ui.toggle_quick_menu)
 
     vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
     vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
