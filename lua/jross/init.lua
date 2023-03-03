201c163d7580166a8778e38ca825fab39c6ad2d7
jross <delacruz.jross@gmail.com>
2023-03-03 19:10:03 +0800

    added border to Mason and lazy menus

--- lua/jross/init.lua
+++ lua/jross/init.lua
@@ -14,4 +14,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-return require('lazy').setup('plugins')
+return require('lazy').setup('plugins', {
+  ui = {
+    border = "rounded",
+  },
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,44 +1,50 @@
 return {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v1.x',
+  "VonHeikemen/lsp-zero.nvim",
+  branch = "v1.x",
   dependencies = {
     -- LSP Support
-    { 'neovim/nvim-lspconfig' }, -- Required
-    { 'williamboman/mason.nvim' }, -- Optional
-    { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    { "neovim/nvim-lspconfig" }, -- Required
+    { "williamboman/mason.nvim" }, -- Optional
+    { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
     -- Autocompletion
-    { 'hrsh7th/nvim-cmp' }, -- Required
-    { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-    { 'hrsh7th/cmp-buffer' }, -- Optional
-    { 'hrsh7th/cmp-path' }, -- Optional
-    { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-    { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+    { "hrsh7th/nvim-cmp" }, -- Required
+    { "hrsh7th/cmp-nvim-lsp" }, -- Required
+    { "hrsh7th/cmp-buffer" }, -- Optional
+    { "hrsh7th/cmp-path" }, -- Optional
+    { "saadparwaiz1/cmp_luasnip" }, -- Optional
+    { "hrsh7th/cmp-nvim-lua" }, -- Optional
 
     -- Snippets
-    { 'L3MON4D3/LuaSnip' }, -- Required
-    { 'rafamadriz/friendly-snippets' }, -- Optional
+    { "L3MON4D3/LuaSnip" }, -- Required
+    { "rafamadriz/friendly-snippets" }, -- Optional
   },
   config = function()
-    local lsp = require('lsp-zero')
-
-    lsp.preset('recommended')
-
-    lsp.ensure_installed({
-      'cssls',
-      'eslint',
-      'html',
-      'jsonls',
-      'lua_ls',
-      'rust_analyzer',
-      'tsserver',
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+      },
     })
 
-    lsp.configure('lua_ls', {
+    local lsp = require("lsp-zero")
+
+    lsp.preset("recommended")
+
+    lsp.ensure_installed({
+      "cssls",
+      "eslint",
+      "html",
+      "jsonls",
+      "lua_ls",
+      "rust_analyzer",
+      "tsserver",
+    })
+
+    lsp.configure("lua_ls", {
       settings = {
         Lua = {
           diagnostics = {
-            globals = { 'vim' }
+            globals = { "vim" }
           }
         }
       }
@@ -47,10 +53,10 @@ return {
     lsp.set_preferences({
       sugguest_lsp_servers = false,
       sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+        error = "E",
+        warn = "W",
+        hint = "H",
+        info = "I"
       }
     })
 
@@ -58,12 +64,12 @@ return {
     lsp.on_attach(function(client, bufnr)
       local opts = {buffer = bufnr, remap = false}
 
-      vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-      vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set('n', '<leader>ws', function() vim.lsp.buf.workspace_symbol() end, opts)
-      vim.keymap.set('n', '<leader>d', function() vim.diagnostic.open_float() end, opts)
-      vim.keymap.set('n', '<leader>rn', function() vim.lsp.buf.rename() end, opts)
-      vim.keymap.set('n', '<leader>a', function() vim.lsp.buf.code_action() end, opts)
+      vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+      vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+      vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>d", function() vim.diagnostic.open_float() end, opts)
+      vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
       vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
 
     end)
