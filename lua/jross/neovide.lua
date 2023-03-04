b0b709d1763a1336be278fa0c2a95d34874429b2
jross <delacruz.jross@gmail.com>
2023-03-04 19:20:52 +0800

    added keymap to adjust font scale in neovide

--- lua/jross/neovide.lua
+++ lua/jross/neovide.lua
@@ -1,9 +1,5 @@
 if vim.g.neovide then
-
   vim.o.guifont = "FiraCode NF:h14"
-
-  local bg_transparency = 0.75
-  vim.g.neovide_transparency = bg_transparency
 
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_refresh_rate = 144
@@ -12,13 +8,32 @@ if vim.g.neovide then
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_cursor_vfx_mode = "sonicboom"
 
-  vim.keymap.set({"n", "v"}, "<C-v>", [["+P]])
-  vim.keymap.set({"c", "i"}, "<C-v>", "<C-R>+")
-  vim.keymap.set("n", "<leader>bg", function ()
+  vim.keymap.set({ "n", "v" }, "<C-v>", [["+P]])
+  vim.keymap.set({ "c", "i" }, "<C-v>", "<C-R>+")
+
+  -- Background Transparency Toggle
+  local bg_transparency = 0.75
+  vim.g.neovide_transparency = bg_transparency
+  vim.keymap.set("n", "<leader>bg", function()
     if vim.g.neovide_transparency == 1 then
       vim.api.nvim_set_var("neovide_transparency", bg_transparency)
     else
       vim.api.nvim_set_var("neovide_transparency", 1)
     end
+  end)
+
+  -- Font Scale increase/decrease/reset
+  vim.g.neovide_scale_factor = 1.0
+  local change_scale_factor = function(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+  vim.keymap.set("n", "<F10>", function()
+    vim.api.nvim_set_var("neovide_scale_factor", 1)
+  end)
+  vim.keymap.set("n", "<F11>", function()
+    change_scale_factor(1 / 1.25)
+  end)
+  vim.keymap.set("n", "<F12>", function()
+    change_scale_factor(1.25)
   end)
 end
