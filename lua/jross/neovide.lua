f22bc1851e3f751fd9368b0b3ea6c192ed178488
jross <delacruz.jross@gmail.com>
2023-03-04 17:28:54 +0800

    added keymap to toggle neovide background transparency

--- lua/jross/neovide.lua
+++ lua/jross/neovide.lua
@@ -1,6 +1,10 @@
 if vim.g.neovide then
+
   vim.o.guifont = "FiraCode NF:h14"
-  vim.g.neovide_transparency = 0.75
+
+  local bg_transparency = 0.75
+  vim.g.neovide_transparency = bg_transparency
+
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_refresh_rate = 144
   vim.g.neovide_refresh_rate_idle = 1
@@ -10,4 +14,11 @@ if vim.g.neovide then
 
   vim.keymap.set({"n", "v"}, "<C-v>", [["+P]])
   vim.keymap.set({"c", "i"}, "<C-v>", "<C-R>+")
+  vim.keymap.set("n", "<leader>bg", function ()
+    if vim.g.neovide_transparency == 1 then
+      vim.api.nvim_set_var("neovide_transparency", bg_transparency)
+    else
+      vim.api.nvim_set_var("neovide_transparency", 1)
+    end
+  end)
 end
