59d93e3f0fe708bdc3e99deca922ed4b28e9a0c4
jross <delacruz.jross@gmail.com>
2023-03-07 19:22:37 +0800

    adjusted remaps

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -28,9 +28,7 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lprev<CR>zz")
 
-vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
-end)
+vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
 
 vim.keymap.set("n", "<leader>[", vim.cmd.bprev)
 vim.keymap.set("n", "<leader>]", vim.cmd.bnext)
