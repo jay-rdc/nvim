1a05d13f70a7991138dde5b750e0971cceafc5c7
jross <delacruz.jross@gmail.com>
2023-03-11 00:13:42 +0800

    adjusted fugitive kemaps

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,7 +1,7 @@
 return {
   "tpope/vim-fugitive",
   config = function()
-    vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Fugitive: Open git status in new horizontal window" });
-    vim.keymap.set("n", "<leader>gd", vim.cmd.Gdiffsplit, { desc = "Fugitive: Open current git file changes in new vertical window" });
+    vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Fugitive: Git status" })
+    vim.keymap.set("n", "<leader>gs", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/Git merge tool" })
   end
 }
