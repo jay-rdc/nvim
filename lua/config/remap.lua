549f4ac9e671e1489b6bd561eb02ee572950800b
jross <delacruz.jross@gmail.com>
2023-03-10 19:22:28 +0800

    added keymap to change line ending of file

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -24,3 +24,5 @@ vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Go to previous quickf
 
 vim.keymap.set("n", "<leader>[", vim.cmd.bprev, { desc = "Switch to previous buffer" })
 vim.keymap.set("n", "<leader>]", vim.cmd.bnext, { desc = "Switch to next buffer" })
+
+vim.keymap.set("n", "<leader>\\\\", "<cmd>e ++ff=dos<CR>", { desc = "Change line ending of current file to CRLF"})
