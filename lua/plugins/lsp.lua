c6de11267c7a0a454db8e7792dc51e571f9c1c38
jross <delacruz.jross@gmail.com>
2023-03-12 02:30:13 +0800

    removed unused keymap in lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -77,12 +77,11 @@ return {
 
     ---@diagnostic disable-next-line: unused-local
     lsp.on_attach(function(client, bufnr)
-      local opts = {buffer = bufnr, remap = false}
+      local opts = { buffer = bufnr, remap = false }
 
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
       vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
-      vim.keymap.set("n", "<leader>ws", function() vim.lsp.buf.workspace_symbol() end, opts)
       vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
       vim.keymap.set("n", "<leader>rr", function() vim.lsp.buf.references() end, opts)
       vim.keymap.set("n", "<leader>=", function() vim.lsp.buf.format() end, opts)
