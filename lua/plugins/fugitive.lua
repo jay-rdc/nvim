374c93fb79d063d901a1d20727f824724f633b7d
jross <delacruz.jross@gmail.com>
2023-03-12 23:10:04 +0800

    added git blame keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,7 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Fugitive: Git status" })
+    vim.keymap.set("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Fugitive: Git blame" })
     vim.keymap.set("n", "<leader>gs", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/merge tool" })
   end
 }
