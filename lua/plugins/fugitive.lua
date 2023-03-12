11db6954ba1c0255ae8ca0cd3fca5d49ddb31f89
jross <delacruz.jross@gmail.com>
2023-03-12 12:57:20 +0800

    edited fugitive git diff keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -2,6 +2,6 @@ return {
   "tpope/vim-fugitive",
   config = function()
     vim.keymap.set("n", "<leader>gg", vim.cmd.Git, { desc = "Fugitive: Git status" })
-    vim.keymap.set("n", "<leader>gs", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/Git merge tool" })
+    vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/merge tool" })
   end
 }
