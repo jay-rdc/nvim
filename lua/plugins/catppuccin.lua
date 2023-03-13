670e99989ca3ad42cd0c3176cf09111c3401db3e
jross <delacruz.jross@gmail.com>
2023-03-13 20:11:13 +0800

    edited catppuccin highlight groups

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -7,10 +7,12 @@ return {
       transparent_background = true,
       custom_highlights = function (colors)
         return {
+          LineNr = { fg = colors.surface2 },
           StatusLineSectionA = { fg = colors.crust, bg = colors.pink },
           StatusLineSectionB = { fg = colors.pink, bg = colors.surface0 },
           StatusLineSectionANC = { fg = colors.crust, bg = colors.overlay0 },
           StatusLineSectionBNC = { fg = colors.overlay0, bg = colors.surface0 },
+          TelescopeResultsNormal = { fg = colors.subtext0 },
         }
       end
     }
