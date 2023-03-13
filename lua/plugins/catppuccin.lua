4d9a6360dd5208c8db33c9a81e36f6733e4204b1
jross <delacruz.jross@gmail.com>
2023-03-13 20:18:32 +0800

    edited catppuccin highlight groups

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -12,7 +12,6 @@ return {
           StatusLineSectionB = { fg = colors.pink, bg = colors.surface0 },
           StatusLineSectionANC = { fg = colors.crust, bg = colors.overlay0 },
           StatusLineSectionBNC = { fg = colors.overlay0, bg = colors.surface0 },
-          TelescopeResultsNormal = { fg = colors.subtext0 },
         }
       end
     }
