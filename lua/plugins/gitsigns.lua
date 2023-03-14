a9c9dca2a4b66393e739be18f6bd28c082aa34fc
jross <delacruz.jross@gmail.com>
2023-03-15 00:46:48 +0800

    added gitsigns blame line keymap

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -27,6 +27,7 @@ return {
         map({ "n", "v" }, "<leader>hr", ":Gitsigns reset_hunk<CR>", { desc = "Gitsigns: Reset hunk" })
         map("n", "<leader>hu", gs.undo_stage_hunk, { desc = "Gitsigns: Undo stage hunk" })
         map("n", "<leader>hp", gs.preview_hunk, { desc = "Gitsigns: Preview hunk" })
+        map("n", "<leader>hb", function() gs.blame_line{ full = true } end, { desc = "Gitsigns: Blame line" })
 
         map({ "o", "x" }, "ih", ":Gitsigns select_hunk<CR>", { desc = "Gitsigns: Select hunk" })
       end
