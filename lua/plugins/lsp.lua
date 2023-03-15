27acd286e26507d495dfd1fb9fc51d2da4d76caf
jross <delacruz.jross@gmail.com>
2023-03-15 14:20:37 +0800

    changed mason icons

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,11 @@ return {
     require("mason").setup({
       ui = {
         border = "rounded",
+        icons = {
+          package_installed = "󰗠",
+          package_pending = "󰳠",
+          package_uninstalled = "󰝦",
+        }
       },
     })
 
