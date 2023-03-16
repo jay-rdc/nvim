faff6bfd0d4e1e745daef11383b077b962c5cbdb
jross <delacruz.jross@gmail.com>
2023-03-16 21:27:18 +0800

    added fugitive git log keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return {
   config = function()
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Fugitive: Git status" })
     vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/merge tool" })
+    vim.keymap.set("n", "<leader>gl", "<cmd>G log<CR>", { desc = "Fugitive: Git log" })
   end
 }
