956fe4adbae1bf68c2c6484c73559eb738a2dc58
jross <delacruz.jross@gmail.com>
2023-03-21 02:25:23 +0800

    adjusted catppuccin setup config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,23 +2,23 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
-  config = function ()
-    require("catppuccin").setup({
-      flavour = "mocha",
-      transparent_background = true,
-      custom_highlights = function (colors)
-        return {
-          LineNr = { fg = colors.surface2 },
-          StatusLineSectionA = { fg = colors.crust, bg = colors.pink },
-          StatusLineSectionB = { fg = colors.pink, bg = colors.surface0 },
-          StatusLineSectionANC = { fg = colors.crust, bg = colors.overlay0 },
-          StatusLineSectionBNC = { fg = colors.overlay0, bg = colors.surface0 },
-          TelescopeMatching = { fg = colors.peach },
-          TelescopeSelection = { fg = colors.text, bold = true },
-          TelescopeResultsNormal = { fg = colors.overlay2 },
-        }
-      end
-    })
-    vim.cmd("colorscheme catppuccin")
+  opts = {
+    flavour = "mocha",
+    transparent_background = true,
+    custom_highlights = function(colors)
+      return {
+        LineNr = { fg = colors.surface2 },
+        StatusLineSectionA = { fg = colors.crust, bg = colors.pink },
+        StatusLineSectionB = { fg = colors.pink, bg = colors.surface0 },
+        StatusLineSectionANC = { fg = colors.crust, bg = colors.overlay0 },
+        StatusLineSectionBNC = { fg = colors.overlay0, bg = colors.surface0 },
+        TelescopeMatching = { fg = colors.peach },
+        TelescopeSelection = { fg = colors.text, bold = true },
+        TelescopeResultsNormal = { fg = colors.overlay2 },
+      }
+    end
+  },
+  config = function()
+    vim.cmd.colorscheme("catppuccin")
   end
 }
