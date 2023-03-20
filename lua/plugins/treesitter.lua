ddde69727dca53e4d64a89737f45825ebf2019b6
jross <delacruz.jross@gmail.com>
2023-03-21 04:02:48 +0800

    adjusted treesitter setup config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -11,28 +11,27 @@ return {
     "TSUpdateSync",
     "TSUpdate",
   },
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "css",
-        "diff",
-        "gitcommit",
-        "gitignore",
-        "help",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "rust",
-        "typescript",
-      },
-      sync_install = false,
-      auto_install = true,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end,
+  main = "nvim-treesitter.configs",
+  opts = {
+    ensure_installed = {
+      "css",
+      "diff",
+      "gitcommit",
+      "gitignore",
+      "help",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "rust",
+      "typescript",
+    },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+  },
 }
