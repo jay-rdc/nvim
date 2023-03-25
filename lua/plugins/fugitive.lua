42507e3667ec81d8cc30da38234679c4819bd43c
jross <delacruz.jross@gmail.com>
2023-03-25 23:12:07 +0800

    removed fugitive git push keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,8 +4,5 @@ return {
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Fugitive: Git status" })
     vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/merge tool" })
     vim.keymap.set("n", "<leader>gl", "<cmd>G log<CR>", { desc = "Fugitive: Git log" })
-    if vim.o.ft == "fugitive" then
-      vim.keymap.set("n", "<leader>gp", "<cmd>G push<CR>", { desc = "Fugitive: Git push" })
-    end
   end
 }
