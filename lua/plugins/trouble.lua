b3f527ab8d0b0eda2ce36a24c09310006cd8a3db
jross <delacruz.jross@gmail.com>
2023-03-26 18:52:15 +0800

    edited Trouble keymap descriptions

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,8 +1,8 @@
 return {
   "folke/trouble.nvim",
   keys = {
-    { mode = "n", "<leader>tr", "<cmd>TroubleToggle lsp_references<CR>", desc = "Trouble: Toggle Trouble LSP references menu" },
-    { mode = "n", "<leader>td", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Trouble: Toggle Trouble diagnostics menu" },
+    { mode = "n", "<leader>tr", "<cmd>TroubleToggle lsp_references<CR>", desc = "Trouble: Toggle LSP references" },
+    { mode = "n", "<leader>td", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Trouble: Toggle diagnostics" },
   },
   opts = {
     signs = {
