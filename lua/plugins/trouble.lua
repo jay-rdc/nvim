6878c702f04b7dd5be0ac9088d67c2848240e634
jross <delacruz.jross@gmail.com>
2023-03-26 15:36:01 +0800

    changed diagnostic info sign in trouble menu

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,4 +4,9 @@ return {
     { mode = "n", "<leader>tr", "<cmd>TroubleToggle lsp_references<CR>", desc = "Trouble: Toggle Trouble LSP references menu" },
     { mode = "n", "<leader>td", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Trouble: Toggle Trouble diagnostics menu" },
   },
+  opts = {
+    signs = {
+      information = "󰋽",
+    },
+  },
 }
