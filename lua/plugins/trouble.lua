45773c173ca023e640d7e761a78dcd6c022b9581
jross <delacruz.jross@gmail.com>
2023-03-26 13:44:13 +0800

    added trouble.nvim plugin

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { mode = "n", "<leader>tr", "<cmd>TroubleToggle lsp_references<CR>", desc = "Trouble: Toggle Trouble LSP references menu" },
+    { mode = "n", "<leader>td", "<cmd>TroubleToggle workspace_diagnostics<CR>", desc = "Trouble: Toggle Trouble diagnostics menu" },
+  },
+}
