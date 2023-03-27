4d34c5584975210045d370909eb83310ab3dc09c
jross <delacruz.jross@gmail.com>
2023-03-27 20:12:55 +0800

    used default mappings for comment.nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,19 +1,5 @@
 return {
   "numToStr/Comment.nvim",
   event = { "BufNewFile", "BufReadPre" },
-  opts = {
-    toggler = {
-      line = "<C-_><C-_>",
-      block = "<C-b><C-b>",
-    },
-    opleader = {
-      line = "<C-_>",
-      block = "<C-b>",
-    },
-    extra = {
-      above = "<C-_>O",
-      below = "<C-_>o",
-      eol = "<C-_>A",
-    },
-  }
+  opts = {},
 }
