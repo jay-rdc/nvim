b5cf7611d44acec101c99e6fdbd86cedb27c332d
Ross <delacruz.jross@gmail.com>
2023-03-31 00:18:57 +0800

    removed git diff component in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -14,17 +14,6 @@ return {
       }
     }
 
-    local function diff_source()
-      local gitsigns = vim.b.gitsigns_status_dict
-      if gitsigns then
-        return {
-          added = gitsigns.added,
-          modified = gitsigns.changed,
-          removed = gitsigns.removed,
-        }
-      end
-    end
-
     require("lualine").setup({
       options = {
         theme = custom_theme,
@@ -53,10 +42,6 @@ return {
             icon = "󰘬",
           },
           {
-            "diff",
-            source = diff_source,
-          },
-          {
             "diagnostics",
             sources = { "nvim_diagnostic" },
           },
@@ -83,10 +68,6 @@ return {
           {
             "FugitiveHead",
             icon = "󰘬",
-          },
-          {
-            "diff",
-            source = diff_source,
           },
           {
             "diagnostics",
