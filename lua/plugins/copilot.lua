01f7d9eeb5769fe0909b4182577a4b4ba8892232
Ross <delacruz.jross@gmail.com>
2023-04-01 01:28:47 +0800

    feat(copilot): added TelescopePrompt in disabled filetypes

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,10 @@
 return {
   "github/copilot.vim",
   config = function()
+    vim.g.copilot_filetypes = {
+      TelescopePrompt = false,
+    }
+
     vim.keymap.set("n", "<leader>cp", "<cmd>Copilot panel<CR>", { desc = "Copilot: Open panel" })
   end
 }
