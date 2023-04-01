0f388ae180fadf98d8fd3acb6bf3a10729a18aa7
Ross <delacruz.jross@gmail.com>
2023-04-01 19:46:20 +0800

    feat(fugitive): added keymap to show git log of repo

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return {
     vim.keymap.set("n", "<leader>gs", vim.cmd.Git, { desc = "Fugitive: Git status" })
     vim.keymap.set("n", "<leader>gd", "<cmd>Gvdiffsplit!<CR>", { desc = "Fugitive: Git diff/merge tool" })
     vim.keymap.set("n", "<leader>gl", "<cmd>G log %<CR>", { desc = "Fugitive: Git log of current file" })
+    vim.keymap.set("n", "<leader>gL", "<cmd>G log<CR>", { desc = "Fugitive: Git log of repo" })
   end
 }
