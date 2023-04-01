e013753e4d4c730c8c7cc0d2934d42b2fe40a9c4
Ross <delacruz.jross@gmail.com>
2023-04-01 19:48:03 +0800

    feat(copilot): remapped copilot#Accept()

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,7 +4,10 @@ return {
     vim.g.copilot_filetypes = {
       TelescopePrompt = false,
     }
+    vim.g.copilot_no_tab_map = true
 
+    vim.keymap.set("i", "<Tab>", "<Tab>")
+    vim.api.nvim_set_keymap("i", "<M-l>", [[copilot#Accept("")]], { expr = true, silent = true, desc = "Copilot: Accept" })
     vim.keymap.set("n", "<leader>cp", "<cmd>Copilot panel<CR>", { desc = "Copilot: Open panel" })
   end
 }
