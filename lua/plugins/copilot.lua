b41dbb45886a1ec112e9ce203ed0434eae51b820
Ross <delacruz.jross@gmail.com>
2023-04-01 14:58:30 +0800

    revert(copilot): removed custom keymap for copilot#Accept()

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -4,10 +4,7 @@ return {
     vim.g.copilot_filetypes = {
       TelescopePrompt = false,
     }
-    vim.g.copilot_no_tab_map = true
 
-    vim.keymap.set("i", "<Tab>", "<Tab>")
-    vim.keymap.set("i", "<M-l>", [[copilot#Accept("")]], { expr = true, silent = true, desc = "Copilot: Accept" })
     vim.keymap.set("n", "<leader>cp", "<cmd>Copilot panel<CR>", { desc = "Copilot: Open panel" })
   end
 }
