7fe02fda36c14bd83d16a4e7518afe6cea5c2271
Ross <delacruz.jross@gmail.com>
2023-04-03 00:28:58 +0800

    feat: added ftplugin for .txt files

---
+++ after/ftplugin/text.lua
@@ -0,0 +1,4 @@
+vim.opt.wrap = true
+
+vim.keymap.set("n", "j", "gj", { buffer = 0, desc = "Move to next visual line" })
+vim.keymap.set("n", "k", "gk", { buffer = 0, desc = "Move to previous visual line" })
