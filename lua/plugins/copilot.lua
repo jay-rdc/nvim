8fb31bbc0937cac01d982ae739c73f65b9ecc9e5
jay-rdc <delacruz.jross@gmail.com>
2023-04-20 06:11:17 +0800

    feat(copilot): remove `:Copilot panel` keymap

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -9,6 +9,5 @@ return {
 
     vim.keymap.set("i", "<Tab>", "<Tab>")
     vim.api.nvim_set_keymap("i", "<M-l>", [[copilot#Accept("")]], { expr = true, silent = true, desc = "Copilot: Accept" })
-    vim.keymap.set("n", "<leader>cp", "<cmd>Copilot panel<CR>", { desc = "Copilot: Open panel" })
   end
 }
