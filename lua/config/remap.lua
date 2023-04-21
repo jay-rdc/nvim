30635a3fd636fce0b1e77afdad422e2e28195938
jay-rdc <delacruz.jross@gmail.com>
2023-04-21 22:19:46 +0800

    style(remap): move keymaps without description at bottom

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -16,9 +16,6 @@ vim.keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboar
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank text to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete text without yanking deleted text" })
 
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "Q", "<nop>")
-
 vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz", { desc = "Go to next quickfix list item" })
 vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz", { desc = "Go to previous quickfix list item" })
 
@@ -26,3 +23,6 @@ vim.keymap.set("n", "[b", vim.cmd.bprev, { desc = "Switch to previous buffer" })
 vim.keymap.set("n", "]b", vim.cmd.bnext, { desc = "Switch to next buffer" })
 
 vim.keymap.set("v", "<leader>gab", [[:g/^<\{7}\|^|\{7}\|^=\{7}\|^>\{7}/d<CR>]], { desc = "Git merge conflict: Accept both changes" })
+
+vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set("n", "Q", "<nop>")
