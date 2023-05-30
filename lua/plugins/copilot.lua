18ca9b9e09d10d83eb76addf01a4ed4b24d585f0
jay-rdc <delacruz.jross@gmail.com>
2023-05-31 01:22:26 +0800

    feat(copilot): comment out plugin setup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,14 +1,13 @@
 return {
-  "github/copilot.vim",
-  enabled = false,
-  config = function()
-    vim.g.copilot_filetypes = {
-      TelescopePrompt = false,
-      text = false,
-    }
-    vim.g.copilot_no_tab_map = true
-
-    vim.keymap.set("i", "<Tab>", "<Tab>")
-    vim.api.nvim_set_keymap("i", "<Right>", [[copilot#Accept("<Right>")]], { expr = true, silent = true, desc = "Copilot: Accept" })
-  end
+--   "github/copilot.vim",
+--   config = function()
+--     vim.g.copilot_filetypes = {
+--       TelescopePrompt = false,
+--       text = false,
+--     }
+--     vim.g.copilot_no_tab_map = true
+--
+--     vim.keymap.set("i", "<Tab>", "<Tab>")
+--     vim.api.nvim_set_keymap("i", "<Right>", [[copilot#Accept("<Right>")]], { expr = true, silent = true, desc = "Copilot: Accept" })
+--   end
 }
