20c864d0ec856d3afa90c68198c7dc32e27c03ea
jay-rdc <delacruz.jross@gmail.com>
2023-09-06 16:03:45 +0800

    feat: enable copilot plugin

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,13 +1,13 @@
 return {
---   "github/copilot.vim",
---   config = function()
---     vim.g.copilot_filetypes = {
---       TelescopePrompt = false,
---       text = false,
---     }
---     vim.g.copilot_no_tab_map = true
---
---     vim.keymap.set("i", "<Tab>", "<Tab>")
---     vim.api.nvim_set_keymap("i", "<Right>", [[copilot#Accept("<Right>")]], { expr = true, silent = true, desc = "Copilot: Accept" })
---   end
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_filetypes = {
+      TelescopePrompt = false,
+      text = false,
+    }
+    vim.keymap.set("i", "<Tab>", "<Tab>")
+    vim.api.nvim_set_keymap("i", "<C-j>", [[copilot#Accept("<Right>")]], { expr = true, silent = true, desc = "Copilot: Accept" })
+
+    vim.g.copilot_no_tab_map = true
+  end
 }
