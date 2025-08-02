92470e3fb38c2cce5af6eae22b1afcb89562e9e6
jay-rdc <delacruz.jross@gmail.com>
2025-08-02 14:09:44 +0800

    feat(catppuccin): add transparent floating windows config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,6 +5,7 @@ return {
   opts = {
     flavour = "mocha",
     transparent_background = true,
+    float = { transparent = true },
     custom_highlights = function(colors)
       return {
         CopilotSuggestion = { link = "Comment" },
